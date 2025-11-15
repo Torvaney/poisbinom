b165802b707310bc77e7c77032927157ed3f0356
Ben Torvaney <torvaney@protonmail.com>
2025-11-15 10:34:00 +0000

    Fix R symbols in init.c to match RcppExports
    Update extern declarations and CallEntries in R_init_poisbinom to
    use the leading underscores generated by Rcpp::compileAttributes().
    This resolves the "_poisbinom_dpoisbinom not found" error
    and ensures the package loads correctly.

--- src/init.c
+++ src/init.c
@@ -3,21 +3,21 @@
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-/* FIXME: 
+/* FIXME:
    Check these declarations against the C/Fortran source code.
 */
 
 /* .Call calls */
-extern SEXP poisbinom_dpoisbinom(SEXP, SEXP, SEXP);
-extern SEXP poisbinom_ppoisbinom(SEXP, SEXP, SEXP, SEXP);
-extern SEXP poisbinom_qpoisbinom(SEXP, SEXP, SEXP, SEXP);
-extern SEXP poisbinom_rpoisbinom(SEXP, SEXP);
+extern SEXP _poisbinom_dpoisbinom(SEXP, SEXP, SEXP);
+extern SEXP _poisbinom_ppoisbinom(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _poisbinom_qpoisbinom(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _poisbinom_rpoisbinom(SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"poisbinom_dpoisbinom", (DL_FUNC) &poisbinom_dpoisbinom, 3},
-    {"poisbinom_ppoisbinom", (DL_FUNC) &poisbinom_ppoisbinom, 4},
-    {"poisbinom_qpoisbinom", (DL_FUNC) &poisbinom_qpoisbinom, 4},
-    {"poisbinom_rpoisbinom", (DL_FUNC) &poisbinom_rpoisbinom, 2},
+    {"_poisbinom_dpoisbinom", (DL_FUNC) &_poisbinom_dpoisbinom, 3},
+    {"_poisbinom_ppoisbinom", (DL_FUNC) &_poisbinom_ppoisbinom, 4},
+    {"_poisbinom_qpoisbinom", (DL_FUNC) &_poisbinom_qpoisbinom, 4},
+    {"_poisbinom_rpoisbinom", (DL_FUNC) &_poisbinom_rpoisbinom, 2},
     {NULL, NULL, 0}
 };
 
